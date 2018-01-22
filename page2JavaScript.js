59105fb97efa4cf78c5285fb349a5604ce7d2152
Tarek-Essam <tarekessam1311@gmail.com>
2018-01-23 00:27:35 +0200

    update

--- page2JavaScript.js
+++ page2JavaScript.js
@@ -1,7 +1,8 @@
 document.getElementById("products").addEventListener("click", addOrder);
 document.getElementById("panel").addEventListener("input", increaseOrder);
+document.getElementById("panel").addEventListener("click", remove);
 let orders = document.getElementById('orders');
-let numberOfOrders = 0;
+let totalOrders = {};
 
 function addOrder(event){
     if(event.target.tagName === "IMG"){
@@ -16,7 +17,9 @@ function addOrder(event){
                 let sum = ordersNames[i].children[2].textContent;
                 sum = sum.split(" ");
                 sum = Number(sum[1]) + Number(product[1]);
+                totalOrders[`${product[0]}`] = sum;
                 ordersNames[i].children[2].innerHTML = "EPG " + sum;
+                computeCheck();
                 return;
             }
         }
@@ -25,29 +28,69 @@ function addOrder(event){
         let orderName = document.createElement('p');
         let amount = document.createElement("input");
         let price = document.createElement("p");
+        let remove = document.createElement("button");
 
         amount.type = "number";
         amount.value = "1";
         amount.min = "1";
         amount.onkeydown="return false";
+
         orderName.textContent = product[0];
-        orderName.setAttribute("name", product[1]);
-        order.classList.add("ordersnames");
+        orderName.setAttribute("name", event.target.name);
         orderName.style.display = "inline-block";
+
         price.style.display = "inline-block";
         price.textContent = "EPG " + product[1];
+
+        remove.textContent = "remove";
+        remove.id = product[0];
+
+        order.classList.add("ordersnames");
+
         order.appendChild(orderName);
         order.appendChild(amount);
         order.appendChild(price);
+        order.appendChild(remove);
         orders.appendChild(order);
+
+        totalOrders[`${product[0]}`] = product[1];
+        computeCheck();
+
+        document.getElementById('notes').style.display = "block";
+        document.getElementById('room').style.display = "block";
+
     }
+}
+
+function remove(event){
+    if(event.target.tagName === "BUTTON"){        
+        let order = document.getElementById(event.target.getAttribute("id"));
+        order = order.parentElement;
+        let orderName = order.children[0];
+        orderName = orderName.getAttribute("name");
+        orderName = orderName.split("-");
+        delete totalOrders[`${orderName[0]}`];
+        orders.removeChild(order);
+        computeCheck();
+    }
+
 }
 
 function increaseOrder(event){
     let newNumber = event.target.value;
     let order = event.target.parentElement.children;
-    let price = order[0].getAttribute("name");
-    let sum = Number(price) * Number(newNumber);
-    order[2].innerHTML = "EPG " + sum;  
+    let product = order[0].getAttribute("name");
+    product = product.split("-");
+    let sum = Number(product[1]) * Number(newNumber);
+    totalOrders[`${product[0]}`] = sum;
+    computeCheck();
+    order[2].innerHTML = "EPG " + sum;
+}
 
+function computeCheck() {
+    let totalPrice = 0;
+    for (let price in totalOrders){
+        totalPrice += Number(totalOrders[price]);
+    }
+    document.getElementById("totalprice").textContent = "EPG " + totalPrice;
 }
