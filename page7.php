93129c562f43b5cc7be9be279f0a8495f23e69ca
Mina Nasr <mina@localhost.localdomain>
2018-01-22 20:39:42 +0200

    html files of page7,8->Mina

---
+++ page7.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>page7</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="page7.css">
+
+</head>
+<body>
+
+
+	  <div class="nav">
+	  	<a href="#">Home</a> |
+	  	<a href="#">Products</a> |
+	  	<a href="#">Users</a> |
+	  	<a href="#">Manual Order</a> |
+	  	<a href="#">Checks</a>
+	  </div>
+	  <div class="admin">
+	  	<img src="profile.png" width=50 height=50>
+	  	<h3>Admin</h3>
+	  </div>
+	  <br><br>
+	  <div class="container">
+	  	<form method="post" action="#">
+	  		<h3>Add User</h3>
+	  		<br><br>
+	  		Name<input type="text" name="name" class="input" id="name-input"><br><br>
+	  		Email<input type="text" name="email" class="input" id="email-input"><br><br>
+	  		Password<input type="text" name="password" class="input" id="password-input"><br><br>
+	  		Confirm Password<input type="text" name="confirm" class="input" id="confirm-input"><br><br>
+	  		Room No.<input type="text" name="room no." class="input" id="room-input"><br><br>
+	  		Ext.<input type="text" name="ext" class="input" id="ext-input"><br><br>
+	  		Profile Picture<input type="file" name="profile pic" class="input" id="profile-pic-input"><br><br>
+	  		<button type="submit" name="submit" class="button">Submit</button>
+	  		<button type="reset" name="reset" class="button">Reset</button>
+	  	</form>
+	  	
+	  </div>
+
+</body>
+</html>
