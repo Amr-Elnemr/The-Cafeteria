c51bd1de933ec4f594ae5d03e01525f213dc840e
Mina Nasr <mina@localhost.localdomain>
2018-01-26 13:01:10 +0200

    class user and login page

---
+++ login.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>login form</title>
+	<meta charset="utf-8">
+	<style type="text/css">
+		
+		#form-container{
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<div id="form-container">
+		<form id="form" action="class-user.php" method="POST">
+			<div id="header">
+				<h2>Login Form</h2>
+			</div>
+			<div id="body">
+				<input type="email" name="email" placeholder=" Email" >
+				<!-- <span style="color: red"><?php echo $email_error ?></span> -->
+				<br><br>
+				<input type="password" name="password" placeholder="Password">
+				<!-- <span style="color: red"><?php echo $password_error ?></span> -->
+			</div>
+			<div id="footer">
+				<button type="submit" id="submit" name="submit">Submit</button>
+				<button type="reset" id="reset" name="reset">Reset</button>
+			</div>
+		</form>
+	</div>
+</body>
+</html>
+
+
+
